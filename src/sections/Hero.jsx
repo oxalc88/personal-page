c78bb8918d7aa2ccffdf98787ab4db9854590697
Rolando Oxalc <rolando.oxalc@gmail.com>
2022-11-09 13:15:35 -0500

    agregado link de chakra

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,4 @@
-import { Box, Center, Circle, Heading, Highlight, Image, Stack, Text } from "@chakra-ui/react"
+import { Box, Center, Circle, Heading, Highlight, Image, Link, Stack, Text } from "@chakra-ui/react"
 import muestra from '../assets/myAvatar.png'
 
 const Hero = () => {
